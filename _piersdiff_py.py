83d38214dad5057a39bd2dba6a1ad5fa0843ff12
Piers Titus van der Torren <pierstitus@gmail.com>
2016-05-11 15:26:43 +0200

    Clean up and rename to Klondike
    Klondike is a version of Patience, and brings the gold to the surface in
    diffs with many junk changes.

--- _piersdiff_py.py
+++ _piersdiff_py.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2005 Bram Cohen, Copyright (C) 2005, 2006 Canonical Ltd
+# Copyright (C) 2016 Piers Titus van der Torren
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,15 +19,9 @@ from __future__ import absolute_import
 
 from bisect import bisect
 import difflib
-
 import re
 
-from bzrlib.trace import mutter
-
-
-__all__ = ['PatienceSequenceMatcher', 'unified_diff', 'unified_diff_files']
-
-
+# function copied from Bram Cohen's Patience diff from bzr
 def unique_lcs_py(a, b):
     """Find the longest common subset for unique lines.
 
@@ -106,95 +100,6 @@ def unique_lcs_py(a, b):
     return result
 
 
-def recurse_matches_py(a, b, alo, blo, ahi, bhi, answer, maxrecursion):
-    """Find all of the matching text in the lines of a and b.
-
-    :param a: A sequence
-    :param b: Another sequence
-    :param alo: The start location of a to check, typically 0
-    :param ahi: The start location of b to check, typically 0
-    :param ahi: The maximum length of a to check, typically len(a)
-    :param bhi: The maximum length of b to check, typically len(b)
-    :param answer: The return array. Will be filled with tuples
-                   indicating [(line_in_a, line_in_b)]
-    :param maxrecursion: The maximum depth to recurse.
-                         Must be a positive integer.
-    :return: None, the return value is in the parameter answer, which
-             should be a list
-
-    """
-    if maxrecursion < 0:
-        mutter('max recursion depth reached')
-        # this will never happen normally, this check is to prevent DOS attacks
-        return
-    oldlength = len(answer)
-    if alo == ahi or blo == bhi:
-        return
-    last_a_pos = alo-1
-    last_b_pos = blo-1
-    for apos, bpos in unique_lcs_py(a[alo:ahi], b[blo:bhi]):
-        # recurse between lines which are unique in each file and match
-        apos += alo
-        bpos += blo
-        # Most of the time, you will have a sequence of similar entries
-        if last_a_pos+1 != apos or last_b_pos+1 != bpos:
-            recurse_matches_py(a, b, last_a_pos+1, last_b_pos+1,
-                apos, bpos, answer, maxrecursion - 1)
-        last_a_pos = apos
-        last_b_pos = bpos
-        answer.append((apos, bpos))
-    if len(answer) > oldlength:
-        # find matches between the last match and the end
-        recurse_matches_py(a, b, last_a_pos+1, last_b_pos+1,
-                           ahi, bhi, answer, maxrecursion - 1)
-    elif a[alo] == b[blo]:
-        # find matching lines at the very beginning
-        while alo < ahi and blo < bhi and a[alo] == b[blo]:
-            answer.append((alo, blo))
-            alo += 1
-            blo += 1
-        recurse_matches_py(a, b, alo, blo,
-                           ahi, bhi, answer, maxrecursion - 1)
-    elif a[ahi - 1] == b[bhi - 1]:
-        # find matching lines at the very end
-        nahi = ahi - 1
-        nbhi = bhi - 1
-        while nahi > alo and nbhi > blo and a[nahi - 1] == b[nbhi - 1]:
-            nahi -= 1
-            nbhi -= 1
-        recurse_matches_py(a, b, last_a_pos+1, last_b_pos+1,
-                           nahi, nbhi, answer, maxrecursion - 1)
-        for i in xrange(ahi - nahi):
-            answer.append((nahi + i, nbhi + i))
-
-
-def _collapse_sequences(matches):
-    """Find sequences of lines.
-
-    Given a sequence of [(line_in_a, line_in_b),]
-    find regions where they both increment at the same time
-    """
-    answer = []
-    start_a = start_b = None
-    length = 0
-    for i_a, i_b in matches:
-        if (start_a is not None
-            and (i_a == start_a + length)
-            and (i_b == start_b + length)):
-            length += 1
-        else:
-            if start_a is not None:
-                answer.append((start_a, start_b, length))
-            start_a = i_a
-            start_b = i_b
-            length = 1
-
-    if length != 0:
-        answer.append((start_a, start_b, length))
-
-    return answer
-
-
 def _check_consistency(answer):
     # For consistency sake, make sure all matches are only increasing
     next_a = -1
@@ -208,7 +113,7 @@ def _check_consistency(answer):
         next_b = b + match_len
 
 
-class PatienceSequenceMatcher_py(difflib.SequenceMatcher):
+class KlondikeSequenceMatcher_py(difflib.SequenceMatcher):
     """Compare a pair of sequences using longest common subset."""
 
     _do_check_consistency = True
@@ -223,6 +128,12 @@ class PatienceSequenceMatcher_py(difflib.SequenceMatcher):
     def get_matching_blocks(self):
         """Return list of triples describing matching subsequences.
 
+        In this algorithm blocks are considered matching when there are changes
+        in junk, which is defined as whitespace or 3+ repeating characters.
+
+        This function currently only supports lists of strings, for single string
+        diff difflib is better suited.
+
         Each triple is of the form (i, j, n), and means that
         a[i:i+n] == b[j:j+n].  The triples are monotonically increasing in
         i and in j.
@@ -230,41 +141,13 @@ class PatienceSequenceMatcher_py(difflib.SequenceMatcher):
         The last triple is a dummy, (len(a), len(b), 0), and is the only
         triple with n==0.
 
-        >>> s = PatienceSequenceMatcher(None, "abxcd", "abcd")
+        >>> s = KlondikeSequenceMatcher(None, "abxcd", "abcd")
         >>> s.get_matching_blocks()
         [(0, 0, 2), (3, 2, 2), (5, 4, 0)]
         """
-        # jam 20060525 This is the python 2.4.1 difflib get_matching_blocks
-        # implementation which uses __helper. 2.4.3 got rid of helper for
-        # doing it inline with a queue.
-        # We should consider doing the same for recurse_matches
 
         if self.matching_blocks is not None:
             return self.matching_blocks
-
-        matches = []
-
-        a_ws = [s.strip() for s in self.a]
-        b_ws = [s.strip() for s in self.b]
-
-        recurse_matches_py(a_ws, b_ws, 0, 0,
-                           len(self.a), len(self.b), matches, 10)
-
-        matches = [m for m in matches if self.a[m[0]] == self.b[m[1]]]
-        # Matches now has individual line pairs of
-        # line A matches line B, at the given offsets
-        self.matching_blocks = _collapse_sequences(matches)
-        self.matching_blocks.append( (len(self.a), len(self.b), 0) )
-        if PatienceSequenceMatcher_py._do_check_consistency:
-            if __debug__:
-                _check_consistency(self.matching_blocks)
-
-        return self.matching_blocks
-
-    def get_nearly_matching_blocks(self):
-
-        if self.nearly_matching_blocks is not None:
-            return self.nearly_matching_blocks
 
         # remove whitespace and repeated characters
         clear_junk = re.compile(r'(.)\1*(?=\1{2})|[ \t\r\n]*')
@@ -305,8 +188,8 @@ class PatienceSequenceMatcher_py(difflib.SequenceMatcher):
             while a_ws[apos + end] == b_ws[bpos + end]:
                 end += 1
 
-            # search for additional matches which might not have been found due to not unique lines
-            # just use difflib here, as unique_lcs was not successfull
+            # Search for additional matches which might not have been found due to not unique lines
+            # Use difflib here, as unique_lcs was not successfull this give better chances
             if apos + start > last_a and bpos + start > last_b and apos + bpos + 2 * start > last_a + last_b + 2:
                 in_matches = difflib.SequenceMatcher(None, a_ws[last_a:apos+start], b_ws[last_b:bpos+start]).get_matching_blocks()
                 matches.extend([(a+last_a, b+last_b, s) for a,b,s in in_matches if s])
@@ -332,8 +215,13 @@ class PatienceSequenceMatcher_py(difflib.SequenceMatcher):
                         break;
                 matches[n+1] = (matches[n+1][0] + d, matches[n+1][1] + d, matches[n+1][2] - d)
 
-        self.nearly_matching_blocks = matches
-        return self.nearly_matching_blocks
+        self.matching_blocks = matches
+
+        if KlondikeSequenceMatcher_py._do_check_consistency:
+            if __debug__:
+                _check_consistency(self.matching_blocks)
+
+        return self.matching_blocks
 
     def get_opcodes(self):
         """Return list of 5-tuples describing how to turn a into b.
@@ -368,7 +256,7 @@ class PatienceSequenceMatcher_py(difflib.SequenceMatcher):
             return self.opcodes
         i = j = 0
         self.opcodes = answer = []
-        for ai, bj, size in self.get_nearly_matching_blocks():
+        for ai, bj, size in self.get_matching_blocks():
 
             # invariant:  we've pumped out correct diffs to change
             # a[:i] into b[:j], and the next matching block is
